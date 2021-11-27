5dee4ef40ae8095c83623cb556eaa914c51d2563
sarahwesley <92330391+sarahwesley@users.noreply.github.com>
2021-11-27 12:33:45 -0500

    fetch user data and display on page. catch erros and display netwrok errors

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,11 +3,89 @@ var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     // make a request to the url
-    fetch(apiUrl).then(function(response) {
+    fetch(apiUrl)
+        .then(function(response) {
+        if (response.ok) {
         response.json().then(function(data) {
-            console.log(data);
+            displayRepos(data, user);
         });
+        } else {
+            alert("error: github user not found");
+        }
+    })
+    .catch(function(error) {
+        alert("unable to connect to github");
+    });
+
+        // check if api returend any repos
+        if (repos.length === 0) {
+            repoContainerEl.textContent = "No repositories found.";
+            return;
+        }
     });
 };
 
 getUserRepos();
+
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // get value from input element
+var username = nameInputEl.value.trim();
+
+if (username) {
+  getUserRepos(username);
+  nameInputEl.value = "";
+} else {
+  alert("Please enter a GitHub username");
+}
+    console.log(event);
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
+
+var displayRepos = function(repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
+
+    // loop over repos
+    for (var i = 0; i < repos.length; i++) {
+        // format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+    
+        // create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+    
+        // create a span element to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        // create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        // check if current repo has issues or not
+        if (repos[i].open_issues_count > 0) {
+        statusEl.innerHTML =
+            "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        } else {
+        statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+
+        // append to container
+        repoEl.appendChild(statusEl);
+    
+        // append to container
+        repoEl.appendChild(titleEl);
+    
+        // append container to the dom
+        repoContainerEl.appendChild(repoEl);
+    }     
+
+    // clear old context
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+};
